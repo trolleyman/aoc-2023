c89bde823f5e7753a4c657af653d14598a1867ee
Callum Tolley <cgtrolley@gmail.com>
2023-12-10 14:28:56 +0000

    Day 7 pt 2

--- day7/main.go
+++ day7/main.go
@@ -106,6 +106,26 @@ const (
 	handTypeFiveOfAKind
 )
 
+func (ht HandType) String() string {
+	switch ht {
+	case handTypeHighCard:
+		return "HighCard"
+	case handTypeOnePair:
+		return "OnePair"
+	case handTypeTwoPair:
+		return "TwoPair"
+	case handTypeThreeOfAKind:
+		return "ThreeOfAKind"
+	case handTypeFullHouse:
+		return "FullHouse"
+	case handTypeFourOfAKind:
+		return "FourOfAKind"
+	case handTypeFiveOfAKind:
+		return "FiveOfAKind"
+	}
+	return "Unknown"
+}
+
 type Hand struct {
 	Cards    [5]Card
 	HandType HandType
@@ -120,58 +140,159 @@ func All[T any](vs []T, f func(T) bool) bool {
 	return true
 }
 
-func getHandType(cards [5]Card) HandType {
-	countsMap := make(map[Card]int)
-	for _, card := range cards {
-		countsMap[card] += 1
+func getCounts[T comparable](values []T) map[T]int {
+	counts := make(map[T]int)
+	for _, value := range values {
+		counts[value] += 1
 	}
+	return counts
+}
+
+func isOfAKind(n int, cards []Card, jackIsJoker bool) (found bool, part []Card, rest []Card) {
+	countsMap := getCounts(cards)
 	var counts []t.T2[Card, int]
 	for k, v := range countsMap {
 		counts = append(counts, t.New2(k, v))
 	}
 	slices.SortFunc(counts, func(a, b t.T2[Card, int]) int { return b.V2 - a.V2 })
-	switch len(counts) {
-	case 1:
-		return handTypeFiveOfAKind
-	case 2:
-		switch counts[0].V2 {
-		case 4:
-			return handTypeFourOfAKind
-		case 3:
-			return handTypeFullHouse
-		}
-		panic("invalid state")
-	case 3:
-		switch counts[0].V2 {
-		case 3:
-			return handTypeThreeOfAKind
-		case 2:
-			if counts[1].V2 != 2 {
-				panic("invalid state")
+
+	for _, t := range counts {
+		card := t.V1
+		cardCount := t.V2
+		if cardCount >= n {
+			for i := 0; i < n; i++ {
+				part = append(part, card)
 			}
-			return handTypeTwoPair
+			for i := 0; i < cardCount-n; i++ {
+				rest = append(rest, card)
+			}
+			for _, otherCard := range cards {
+				if otherCard != card {
+					rest = append(rest, otherCard)
+				}
+			}
+			return true, part, rest
 		}
-		panic("invalid state")
-	case 4:
-		if counts[0].V2 != 2 {
-			panic("invalid state")
+		if !jackIsJoker {
+			continue
 		}
-		return handTypeOnePair
-	case 5:
-		return handTypeHighCard
+
+		// Check for jokers
+		jokerCount := countsMap[cardJ]
+		jokersNeeded := n - cardCount
+		if jokerCount < jokersNeeded {
+			continue
+		}
+
+		for i := 0; i < cardCount; i++ {
+			part = append(part, card)
+		}
+		for i := 0; i < jokersNeeded; i++ {
+			part = append(part, cardJ)
+		}
+		for i := 0; i < jokerCount-jokersNeeded; i++ {
+			rest = append(rest, cardJ)
+		}
+		for _, otherCard := range cards {
+			if otherCard != card && otherCard != cardJ {
+				rest = append(rest, otherCard)
+			}
+		}
+		return true, part, rest
 	}
-	panic("invalid state")
+	return false, cards, nil
 }
 
-func createHand(cards [5]Card) Hand {
-	return Hand{Cards: cards, HandType: getHandType(cards)}
+func getHandType(cards []Card, jackIsJoker bool) HandType {
+	if true {
+		// TODO Doesn't work
+		wildCount := 0
+		if jackIsJoker {
+			var newCards []Card
+			for _, card := range cards {
+				if card == cardJ {
+					wildCount++
+				} else {
+					newCards = append(newCards, card)
+				}
+			}
+			cards = newCards
+		}
+		countsMap := getCounts(cards)
+		var counts []t.T2[Card, int]
+		for k, v := range countsMap {
+			counts = append(counts, t.New2(k, v))
+		}
+		slices.SortFunc(counts, func(a, b t.T2[Card, int]) int { return b.V2 - a.V2 })
+		maxCount := wildCount
+		if len(counts) > 0 {
+			maxCount += counts[0].V2
+		}
+		switch len(countsMap) {
+		case 0:
+			return handTypeFiveOfAKind
+		case 1:
+			return handTypeFiveOfAKind
+		case 2:
+			if maxCount == 4 {
+				return handTypeFourOfAKind
+			}
+			return handTypeFullHouse
+		case 3:
+			if maxCount == 3 {
+				return handTypeThreeOfAKind
+			}
+			return handTypeTwoPair
+		case 4:
+			return handTypeOnePair
+		case 5:
+			return handTypeHighCard
+		}
+		panic(fmt.Sprintf("unexpected: cards=%v len(countsMap)=%v wildCount=%v", cards, len(countsMap), wildCount))
+
+	} else {
+		isFiveOfAKind, _, _ := isOfAKind(5, cards[:], jackIsJoker)
+		if isFiveOfAKind {
+			return handTypeFiveOfAKind
+		}
+
+		isFourOfAKind, _, _ := isOfAKind(4, cards[:], jackIsJoker)
+		if isFourOfAKind {
+			return handTypeFourOfAKind
+		}
+
+		isThreeOfAKind, _, restCards := isOfAKind(3, cards[:], jackIsJoker)
+		if isThreeOfAKind {
+			isFullHouse, _, _ := isOfAKind(2, restCards, jackIsJoker)
+			if isFullHouse {
+				return handTypeFullHouse
+			} else {
+				return handTypeThreeOfAKind
+			}
+		}
+
+		isTwoOfAKind, _, restCards := isOfAKind(2, cards[:], jackIsJoker)
+		if isTwoOfAKind {
+			isTwoPair, _, _ := isOfAKind(2, restCards, jackIsJoker)
+			if isTwoPair {
+				return handTypeTwoPair
+			} else {
+				return handTypeOnePair
+			}
+		}
+		return handTypeHighCard
+	}
+}
+
+func createHand(cards [5]Card, jackIsJoker bool) Hand {
+	return Hand{Cards: cards, HandType: getHandType(cards, jackIsJoker)}
 }
 
 func (h Hand) String() string {
-	return fmt.Sprintf("%v%v%v%v%v", h.Cards[0], h.Cards[1], h.Cards[2], h.Cards[3], h.Cards[4])
+	return fmt.Sprintf("%v%v%v%v%v (%v)", h.Cards[0], h.Cards[1], h.Cards[2], h.Cards[3], h.Cards[4], h.HandType)
 }
 
-func parseHand(handString string) (Hand, error) {
+func parseHand(handString string, jackIsJoker bool) (Hand, error) {
 	handString = strings.TrimSpace(handString)
 	var hand []Card
 	for _, r := range handString {
@@ -187,7 +308,7 @@ func parseHand(handString string) (Hand, error) {
 	if len(hand) != 5 {
 		return Hand{}, errors.New("hand not 5 cards")
 	}
-	return createHand([5]Card{hand[0], hand[1], hand[2], hand[3], hand[4]}), nil
+	return createHand([5]Card{hand[0], hand[1], hand[2], hand[3], hand[4]}, jackIsJoker), nil
 }
 
 type HandBid struct {
@@ -195,7 +316,7 @@ type HandBid struct {
 	Bid  int
 }
 
-func getInput(path string) ([]HandBid, error) {
+func getInput(path string, jackIsJoker bool) ([]HandBid, error) {
 	file, err := os.Open(path)
 	if err != nil {
 		return nil, err
@@ -214,7 +335,7 @@ func getInput(path string) ([]HandBid, error) {
 		if len(lineSplit) != 2 {
 			return nil, fmt.Errorf("invalid line (too many fields): %+v", line)
 		}
-		hand, err := parseHand(lineSplit[0])
+		hand, err := parseHand(lineSplit[0], jackIsJoker)
 		if err != nil {
 			return nil, err
 		}
@@ -255,24 +376,43 @@ func parseArgs() (Args, error) {
 	return Args{Part: part, InputPath: os.Args[2]}, nil
 }
 
-func sortHand(hand1 Hand, hand2 Hand) int {
-	handTypeDiff := int(hand1.HandType - hand2.HandType)
-	if handTypeDiff != 0 {
-		return handTypeDiff
-	}
-	for i := range hand1.Cards {
-		card1 := hand1.Cards[i]
-		card2 := hand2.Cards[i]
-		cardDiff := int(card1 - card2)
-		if cardDiff != 0 {
-			return cardDiff
+func getSortCardFunc(jackIsJoker bool) func(Card, Card) int {
+	return func(card1, card2 Card) int {
+		if jackIsJoker {
+			if card1 == cardJ {
+				return -1
+			} else if card2 == cardJ {
+				return 1
+			}
 		}
+		return int(card1 - card2)
 	}
-	return 0
 }
 
-func sortHandBid(handBid1 HandBid, handBid2 HandBid) int {
-	return sortHand(handBid1.Hand, handBid2.Hand)
+func getSortHandFunc(jackIsJoker bool) func(Hand, Hand) int {
+	cardSortFunc := getSortCardFunc(jackIsJoker)
+	return func(hand1, hand2 Hand) int {
+		handTypeDiff := int(hand1.HandType - hand2.HandType)
+		if handTypeDiff != 0 {
+			return handTypeDiff
+		}
+		for i := range hand1.Cards {
+			card1 := hand1.Cards[i]
+			card2 := hand2.Cards[i]
+			cardDiff := cardSortFunc(card1, card2)
+			if cardDiff != 0 {
+				return cardDiff
+			}
+		}
+		return 0
+	}
+}
+
+func getSortHandBidFunc(jackIsJoker bool) func(HandBid, HandBid) int {
+	f := getSortHandFunc(jackIsJoker)
+	return func(handBid1, handBid2 HandBid) int {
+		return f(handBid1.Hand, handBid2.Hand)
+	}
 }
 
 func run() error {
@@ -282,31 +422,27 @@ func run() error {
 	}
 	fmt.Printf("Args: %+v\n", args)
 
-	handBids, err := getInput(args.InputPath)
+	jackIsJoker := args.Part == 2
+	handBids, err := getInput(args.InputPath, jackIsJoker)
 	if err != nil {
 		return err
 	}
 
-	switch args.Part {
-	case 1:
-		// Part 1
-		slices.SortStableFunc(handBids, sortHandBid)
-		totalWinnings := 0
-		for i, handBid := range handBids {
-			rank := i + 1
-			winnings := handBid.Bid * rank
-			totalWinnings += winnings
-			fmt.Printf("Rank %v: %+v wins %v\n", rank, handBid, winnings)
-		}
-		fmt.Printf("Total winnings: %v\n", totalWinnings)
-
-	case 2:
-		// Part 2
-		// TODO
-		panic("NYI")
-	}
+	fmt.Printf("Total winnings: %v\n", getTotalWinnings(handBids, jackIsJoker))
 
 	return nil
+}
+
+func getTotalWinnings(handBids []HandBid, jackIsJoker bool) int {
+	slices.SortStableFunc(handBids, getSortHandBidFunc(jackIsJoker))
+	totalWinnings := 0
+	for i, handBid := range handBids {
+		rank := i + 1
+		winnings := handBid.Bid * rank
+		totalWinnings += winnings
+		fmt.Printf("Rank %v: %+v wins %v\n", rank, handBid, winnings)
+	}
+	return totalWinnings
 }
 
 func main() {
