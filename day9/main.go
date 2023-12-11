a8ea805ae1d179e509a16e61e85c622ee43597cb
Callum Tolley <cgtrolley@gmail.com>
2023-12-11 13:17:06 +0000

    Day 9 pt 1

--- day9/main.go
+++ day9/main.go
@@ -6,8 +6,6 @@ import (
 	"os"
 	"strconv"
 	"strings"
-
-	t "github.com/barweiss/go-tuple"
 )
 
 func parseNumbers(numbersString string) ([]int, error) {
@@ -42,38 +40,13 @@ func getInput(path string, multipleRouters bool) ([][]int, error) {
 		if err != nil {
 			return nil, err
 		}
+		histories = append(histories, history)
 	}
 	if err := scanner.Err(); err != nil {
 		return nil, err
 	}
 
 	return histories, nil
-}
-
-func getNode(nodes map[string]*Node, nodeName string) (*Node, error) {
-	node := nodes[nodeName]
-	if node == nil {
-		return nil, fmt.Errorf("unknown node %+v", nodeName)
-	}
-	return node, nil
-}
-
-func (n Network) isEndNode(node *Node) bool {
-	for _, endNode := range n.Ends {
-		if node == endNode {
-			return true
-		}
-	}
-	return false
-}
-
-func (n Network) areAllEndNodes(nodes []*Node) bool {
-	for _, node := range nodes {
-		if !n.isEndNode(node) {
-			return false
-		}
-	}
-	return true
 }
 
 type Args struct {
@@ -108,87 +81,48 @@ func run() error {
 	fmt.Printf("Args: %+v\n", args)
 
 	multipleRouters := args.Part == 2
-	inputMap, err := getInput(args.InputPath, multipleRouters)
+	histories, err := getInput(args.InputPath, multipleRouters)
 	if err != nil {
 		return err
 	}
 
-	fmt.Printf("Directions (%v): ", len(inputMap.Directions))
-	for _, direction := range inputMap.Directions {
-		fmt.Printf("%v", direction)
+	extrapolatedValueSum := 0
+	for hi, history := range histories {
+		diffs := [][]int{history}
+		for i := 1; ; i++ {
+			prevDiff := diffs[i-1]
+			diff := make([]int, len(prevDiff)-1)
+			isZero := true
+			for j := 0; j < len(diff); j++ {
+				diff[j] = prevDiff[j+1] - prevDiff[j]
+				if diff[j] != 0 {
+					isZero = false
+				}
+			}
+			diffs = append(diffs, diff)
+			if isZero {
+				break
+			}
+			if len(diffs) == 1 {
+				panic("empty diff")
+			}
+		}
+
+		for i := len(diffs) - 2; i >= 0; i-- {
+			currentDiff := diffs[i]
+			prevDiff := diffs[i+1]
+			currentDiff = append(currentDiff, currentDiff[len(currentDiff)-1]+prevDiff[len(prevDiff)-1])
+			diffs[i] = currentDiff
+		}
+
+		for i := 0; i < len(diffs); i++ {
+			fmt.Printf("H%v #%v: %v\n", hi, i, diffs[i])
+		}
+		extrapolatedValueSum += diffs[0][len(diffs[0])-1]
 	}
-	fmt.Println("")
-	totalSteps := getTotalStepsAnalytical(inputMap)
-	fmt.Printf("Total steps: %v\n", totalSteps)
+	fmt.Printf("Extrapolated values sum: %v\n", extrapolatedValueSum)
 
 	return nil
-}
-
-func gcd(a, b int) int {
-	if b == 0 {
-		return a
-	}
-	return gcd(b, a%b)
-}
-
-func lcm(a, b int) int {
-	return a / gcd(a, b) * b
-}
-
-func getTotalStepsAnalytical(inputMap Map) int {
-	totalSteps := 1
-	for i, node := range inputMap.Network.Starts {
-		offset, length, endSteps := getPeriodicity(inputMap, node)
-		fmt.Printf("Ghost %v: start=%v, offset=%v, length=%v /lendir=%v endSteps=%v\n", i, node.Name, offset, length, length/len(inputMap.Directions), endSteps)
-		totalSteps = lcm(totalSteps, length)
-	}
-	return totalSteps
-}
-
-func getTotalSteps(inputMap Map) int {
-	printSteps := true
-	step := 0
-	for nodes := inputMap.Network.Starts; !inputMap.Network.areAllEndNodes(nodes); step++ {
-		direction := inputMap.Directions[step%len(inputMap.Directions)]
-		if printSteps {
-			fmt.Printf("Step %v (%v): ", step, direction)
-		}
-		for i, node := range nodes {
-			if printSteps {
-				if i > 0 {
-					fmt.Print("; ")
-				}
-				fmt.Printf("%v -> ", node.Name)
-			}
-			nodes[i] = node.getNextNode(direction)
-			if printSteps {
-				fmt.Printf("%v", nodes[i].Name)
-			}
-		}
-		if printSteps {
-			fmt.Println("")
-		}
-	}
-	return step
-}
-
-func getPeriodicity(inputMap Map, startNode *Node) (offset int, length int, endSteps []int) {
-	nodeDirectionIndexSteps := make(map[t.T2[*Node, int]]int)
-	for step, node := 0, startNode; ; step++ {
-		directionIndex := step % len(inputMap.Directions)
-		nodeDirectionIndex := t.New2(node, directionIndex)
-		if nodeDirectionIndexStep, found := nodeDirectionIndexSteps[nodeDirectionIndex]; found {
-			// Periodicity found
-			offset = nodeDirectionIndexStep
-			length = step - nodeDirectionIndexStep
-			return
-		}
-		if inputMap.Network.isEndNode(node) {
-			endSteps = append(endSteps, step)
-		}
-		nodeDirectionIndexSteps[nodeDirectionIndex] = step
-		node = node.getNextNode(inputMap.Directions[directionIndex])
-	}
 }
 
 func main() {
